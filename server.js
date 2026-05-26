b3766f06f084d3b04276b5b1c0f31b8f7e0f6bd8
rchintan405 <rchintan405@gmail.com>
2026-05-26 13:36:45 +0530

    setup cors

--- server.js
+++ server.js
@@ -28,7 +28,11 @@ connectDB().then(() => {
 });
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
